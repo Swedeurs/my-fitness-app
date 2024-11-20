60928a8ae8eaaeb420579638e1b49b711adc6fb0
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-20 15:22:17 +0100

    implomented my header file

---
+++ src/app/components/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './header';
+
+
+const Layout = ({ children }: { children: React.ReactNode }) => (
+  <>
+    <Header />
+    <main>{children}</main>
+  </>
+);
+
+export default Layout;
