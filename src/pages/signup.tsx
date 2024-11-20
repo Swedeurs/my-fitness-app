0a0b136f68afbf425eb19d1cf613987fb21203fb
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-20 15:28:02 +0100

    moved signup file

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,56 +1,11 @@
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
+import UserInputForm from "@/app/components/forms/user-inputform";
 
-const userSchema = z.object({
-  age: z.number().min(18, { message: "Must be 18 or older" }),
-  weight: z.number().positive("Weight must be positive"),
-  height: z.number().positive("Height must be positive"),
-  fitnessLevel: z.string().nonempty(),
-  dietaryPreferences: z.string().optional(),
-});
 
-type UserFormData = z.infer<typeof userSchema>;
-
-export default function SignupForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<UserFormData>({
-    resolver: zodResolver(userSchema),
-  });
-
-  const onSubmit = async (data: UserFormData) => {
-    console.log(data);
-  };
-
+export default function SignUp() {
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="number" {...register("age")} placeholder="Age" />
-      <p>{errors.age?.message}</p>
-
-      <input type="number" {...register("weight")} placeholder="Weight" />
-      <p>{errors.weight?.message}</p>
-
-      <input type="number" {...register("height")} placeholder="Height" />
-      <p>{errors.height?.message}</p>
-
-      <input
-        type="text"
-        {...register("fitnessLevel")}
-        placeholder="Fitness Level"
-      />
-      <p>{errors.fitnessLevel?.message}</p>
-
-      <input
-        type="text"
-        {...register("dietaryPreferences")}
-        placeholder="Dietary Preferences (Optional)"
-      />
-      <p>{errors.dietaryPreferences?.message}</p>
-
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <h1>Sign Up</h1>
+      <UserInputForm />
+    </div>
   );
 }
