91b324e98bdcd8bb1c4fa5a2d429af1e4509a00a
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-20 15:21:49 +0100

    made header file with links

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import Link from 'next/link';
 
-interface Props {
-  title: string;
-}
-
-const Header: React.FC<Props> = ({ title }) => {
-  return (
-    <header>
-      <h1>{title}</h1>
-    </header>
-  );
-};
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/signup">Sign Up</Link></li>
+        <li><Link href="/dashboard">Dashboard</Link></li>
+      </ul>
+    </nav>
+  </header>
+);
 
 export default Header;
