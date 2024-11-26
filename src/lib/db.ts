435591f583a5c458d3281ef68367b862eff206f6
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-26 12:40:58 +0100

    made database work

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,4 @@
-import { Pool } from "pg";
+import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-export const db = drizzle(pool);
+export const db = drizzle(process.env.DATABASE_URL!);
+export type Db = typeof db;
