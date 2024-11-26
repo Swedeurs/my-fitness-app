9d0d53ad94e71918bd601f487ac44b76ec46dbe4
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-26 12:42:58 +0100

    implomented notefications

---
+++ src/context/notefications.tsx
@@ -0,0 +1,27 @@
+import { NotificationContextType, Notification } from "@/types";
+import { createContext, useEffect, useState, ReactNode } from "react";
+
+
+const NotificationContext = createContext<NotificationContextType | undefined>(undefined);
+
+export const NotificationProvider = ({ children }: { children: ReactNode }) => {
+  const [notifications, setNotifications] = useState<Notification[]>([]);
+
+  useEffect(() => {
+    fetch(`/api/notifications`)
+      .then((res) => res.json())
+      .then((data) => setNotifications(data));
+  }, []);
+
+  const addNotification = (message: string) => {
+    setNotifications((prev) => [...prev, { notificationId: Date.now(), message, timestamp: new Date().toISOString() }]);
+  };
+
+  return (
+    <NotificationContext.Provider value={{ notifications, addNotification }}>
+      {children}
+    </NotificationContext.Provider>
+  );
+};
+
+export default NotificationContext;
