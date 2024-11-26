b5f3b402c6617f4fad6d139d9e8da2544845146e
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-26 19:49:08 +0100

    removed annoying loading page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -5,14 +5,21 @@ import ClientDashboard from "@/app/components/client-dashboard";
 export default function Dashboard() {
   const { user } = useUser();
 
-  if (!user) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div>
       <h1 className="text-4xl font-bold mb-6">Dashboard</h1>
-      {user.role === "trainer" ? <TrainerDashboard /> : <ClientDashboard />}
+      {user ? (
+        user.role === "trainer" ? (
+          <TrainerDashboard />
+        ) : (
+          <ClientDashboard />
+        )
+      ) : (
+        // Render something in the case where the user is null
+        <p className="text-lg text-gray-500">
+          User information is not available at the moment. Please log in.
+        </p>
+      )}
     </div>
   );
 }
