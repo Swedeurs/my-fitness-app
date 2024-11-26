454a673eda4f374410e594267fa367b7f9b2faad
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-26 16:29:49 +0100

    uppdated the dashbaord back tyo being able to handle both trainer and client

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,17 +1,9 @@
-import UserDashboard from "@/app/components/user-dashboard";
 import { useUser } from "@/hooks/use-user";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import TrainerDashboard from "@/app/components/trainer-dashboard";
+import ClientDashboard from "@/app/components/client-dashboard";
 
 export default function Dashboard() {
   const { user } = useUser();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!user) {
-      router.push("/login");
-    }
-  }, [user, router]);
 
   if (!user) {
     return <div>Loading...</div>;
@@ -20,7 +12,11 @@ export default function Dashboard() {
   return (
     <div>
       <h1 className="text-4xl font-bold mb-6">Dashboard</h1>
-      <UserDashboard />
+      {user.role === "trainer" ? (
+        <TrainerDashboard />
+      ) : (
+        <ClientDashboard />
+      )}
     </div>
   );
 }
